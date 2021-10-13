f8260d8787c833daffd9c912b9e51a803f812423
Darshdeep Hora <darshdeep351@gmail.com>
2021-10-12 18:26:25 -0700

    wip

---
+++ client/src/App.js
@@ -0,0 +1,298 @@
+import React, { Component } from "react";
+import getWeb3 from "./getWeb3";
+import Web3 from "web3";
+
+import "./App.css";
+
+class App extends Component {
+  state = { storageValue: 0, web3: null};
+
+  componentDidMount = async () => {
+    try {
+      // Get network provider and web3 instance.
+      var web3 = new Web3(window.ethereum);
+
+      // Get the contract instance.
+
+      // Set web3, accounts, and contract to the state, and then proceed with an
+      // example of interacting with the contract's methods.
+      this.setState({ web3: web3 }, this.runExample);
+    } catch (error) {
+      // Catch any errors for any of the above operations.
+      alert(
+        `Failed to load web3, accounts, or contract. Check console for details.`,
+      );
+      console.error(error);
+    }
+  };
+
+  runExample = async () => {
+    const web3 = this.state.web3;
+
+    const contractAddress = '0x95aD61b0a150d79219dCF64E1E6Cc01f0B64C4cE';
+    
+    // Update state with the result.
+    this.setState({ storageValue: 5 });
+    console.log('done settingg stete');
+
+    const contract = new web3.eth.Contract([
+      {
+          "constant": true,
+          "inputs": [],
+          "name": "name",
+          "outputs": [
+              {
+                  "name": "",
+                  "type": "string"
+              }
+          ],
+          "payable": false,
+          "stateMutability": "view",
+          "type": "function"
+      },
+      {
+          "constant": false,
+          "inputs": [
+              {
+                  "name": "_spender",
+                  "type": "address"
+              },
+              {
+                  "name": "_value",
+                  "type": "uint256"
+              }
+          ],
+          "name": "approve",
+          "outputs": [
+              {
+                  "name": "",
+                  "type": "bool"
+              }
+          ],
+          "payable": false,
+          "stateMutability": "nonpayable",
+          "type": "function"
+      },
+      {
+          "constant": true,
+          "inputs": [],
+          "name": "totalSupply",
+          "outputs": [
+              {
+                  "name": "",
+                  "type": "uint256"
+              }
+          ],
+          "payable": false,
+          "stateMutability": "view",
+          "type": "function"
+      },
+      {
+          "constant": false,
+          "inputs": [
+              {
+                  "name": "_from",
+                  "type": "address"
+              },
+              {
+                  "name": "_to",
+                  "type": "address"
+              },
+              {
+                  "name": "_value",
+                  "type": "uint256"
+              }
+          ],
+          "name": "transferFrom",
+          "outputs": [
+              {
+                  "name": "",
+                  "type": "bool"
+              }
+          ],
+          "payable": false,
+          "stateMutability": "nonpayable",
+          "type": "function"
+      },
+      {
+          "constant": true,
+          "inputs": [],
+          "name": "decimals",
+          "outputs": [
+              {
+                  "name": "",
+                  "type": "uint8"
+              }
+          ],
+          "payable": false,
+          "stateMutability": "view",
+          "type": "function"
+      },
+      {
+          "constant": true,
+          "inputs": [
+              {
+                  "name": "_owner",
+                  "type": "address"
+              }
+          ],
+          "name": "balanceOf",
+          "outputs": [
+              {
+                  "name": "balance",
+                  "type": "uint256"
+              }
+          ],
+          "payable": false,
+          "stateMutability": "view",
+          "type": "function"
+      },
+      {
+          "constant": true,
+          "inputs": [],
+          "name": "symbol",
+          "outputs": [
+              {
+                  "name": "",
+                  "type": "string"
+              }
+          ],
+          "payable": false,
+          "stateMutability": "view",
+          "type": "function"
+      },
+      {
+          "constant": false,
+          "inputs": [
+              {
+                  "name": "_to",
+                  "type": "address"
+              },
+              {
+                  "name": "_value",
+                  "type": "uint256"
+              }
+          ],
+          "name": "transfer",
+          "outputs": [
+              {
+                  "name": "",
+                  "type": "bool"
+              }
+          ],
+          "payable": false,
+          "stateMutability": "nonpayable",
+          "type": "function"
+      },
+      {
+          "constant": true,
+          "inputs": [
+              {
+                  "name": "_owner",
+                  "type": "address"
+              },
+              {
+                  "name": "_spender",
+                  "type": "address"
+              }
+          ],
+          "name": "allowance",
+          "outputs": [
+              {
+                  "name": "",
+                  "type": "uint256"
+              }
+          ],
+          "payable": false,
+          "stateMutability": "view",
+          "type": "function"
+      },
+      {
+          "payable": true,
+          "stateMutability": "payable",
+          "type": "fallback"
+      },
+      {
+          "anonymous": false,
+          "inputs": [
+              {
+                  "indexed": true,
+                  "name": "owner",
+                  "type": "address"
+              },
+              {
+                  "indexed": true,
+                  "name": "spender",
+                  "type": "address"
+              },
+              {
+                  "indexed": false,
+                  "name": "value",
+                  "type": "uint256"
+              }
+          ],
+          "name": "Approval",
+          "type": "event"
+      },
+      {
+          "anonymous": false,
+          "inputs": [
+              {
+                  "indexed": true,
+                  "name": "from",
+                  "type": "address"
+              },
+              {
+                  "indexed": true,
+                  "name": "to",
+                  "type": "address"
+              },
+              {
+                  "indexed": false,
+                  "name": "value",
+                  "type": "uint256"
+              }
+          ],
+          "name": "Transfer",
+          "type": "event"
+      }
+  ], contractAddress);
+
+    contract.getPastEvents('Transfer', {
+      filter: { from: '0x0000000000000000000000000000000000000000'},
+      fromBlock : 'earliest'
+      //topics : ['0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef', '0x0000000000000000000000000000000000000000']
+    },
+    function(errors, events){
+      events.forEach(el => {
+        console.log(el);
+        console.log('transaction hash: %s', el.transactionHash);
+        console.log(el.returnValues);
+      })
+    });
+    console.log('got past events');
+  };
+
+  render() {
+    if (!this.state.web3) {
+      return <div>Loading Web3, accounts, and contract...</div>;
+    }
+    return (
+      <div className="App">
+        <h1>Good to Go!</h1>
+        <p>Your Truffle Box is installed and ready.</p>
+        <h2>Smart Contract Example</h2>
+        <p>
+          If your contracts compiled and migrated successfully, below will show
+          a stored value of 5 (by default).
+        </p>
+        <p>
+          Try changing the value stored on <strong>line 42</strong> of App.js.
+        </p>
+        <div>The stored value is: {this.state.storageValue}</div>
+      </div>
+    );
+  }
+}
+
+export default App;
